cbc41bf13a06ab386fb57a44d10a65e2405bac84
mohamedabdelaty-98 <abdelatym775@gmail>
2023-11-04 19:49:06 +0200

    update compound

--- DataAccessLayer/Models/Compound.cs
+++ DataAccessLayer/Models/Compound.cs
@@ -27,6 +27,9 @@ namespace DataAccessLayer.Models
         public double Street_area { get; set; }
         public double GreenArea { get; set; }
         public double BuildingArea { get; set; }
+        [Column(TypeName ="nvarchar(150)")]
+        public string? Location { get; set; }
+
         public virtual List<Building>? buildings { get; set; } = new List<Building>();
         public virtual List<LandMarksCompound>? landMarksCompounds { get; set; } = new List<LandMarksCompound>();
         public virtual List<CompoundImage>? imageCompounds { get; set; } = new List<CompoundImage>();
